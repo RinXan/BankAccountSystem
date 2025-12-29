d65fe77dff6e00c6bca77515b058246330fedad0
Rinxan <bikbulatovravil@mail.ru>
2025-12-30 00:04:41 +0300

    Exceptions corrected in TransferService

--- Domain/Services/TransferService.cs
+++ Domain/Services/TransferService.cs
@@ -9,11 +9,13 @@ namespace BankAccountSystem.Domain.Services
 {
     public class TransferService
     {
-        public static void Transfer(BankAccount from, BankAccount to, decimal money)
+        public void Transfer(BankAccount from, BankAccount to, decimal money)
         {
-            if (from.Id == to.Id) throw new ArgumentException("Cannot transfer money from one account to itself");
-            if (money <= 0) throw new ArgumentException("Money for transfer should be more than 0");
-            if (!from.CanWithdraw(money)) throw new ArgumentException($"User {from.Name} does not have enough money");
+            if (from == null) throw new ArgumentNullException(nameof(from), "Transfer objects cannot be null"); 
+            if (to == null) throw new ArgumentNullException(nameof(to), "Transfer objects cannot be null"); 
+            if (from.Id == to.Id) throw new InvalidOperationException("Cannot transfer money from one account to itself");
+            if (money <= 0) throw new ArgumentOutOfRangeException("Money for transfer should be more than 0");
+            if (!from.CanWithdraw(money)) throw new InvalidOperationException($"User {from.Name} does not have enough money");
 
             from.Withdraw(money);
             to.Deposit(money);
--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello");
+﻿using BankAccountSystem.Domain.Accounts;
+using BankAccountSystem.Domain.Services;
+
+BankAccount anna = new SavingsAccount(2, "Anna", 20000);
+BankAccount masha = new CreditAccount(3, "Masha", 100);
+
+TransferService transferService = new TransferService();
+
+anna.Deposit(90000);
+masha.Deposit(100);
+
+transferService.Transfer(anna, masha, 5000);
+
+Console.WriteLine(anna.PrintInfo());
+Console.WriteLine(masha.PrintInfo());
