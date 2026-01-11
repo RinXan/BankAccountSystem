6613e5a0e4debfad440970e6a890b7352ca717d6
Rinxan <bikbulatovravil@mail.ru>
2026-01-11 17:21:08 +0300

    IAccountRepository added

---
+++ Domain/Repositories/IAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankAccountSystem.Domain.Accounts;
+
+namespace BankAccountSystem.Domain.Repositories
+{
+    public interface IAccountRepository
+    {
+        BankAccount GetById(int id);
+        IReadOnlyCollection<BankAccount> GetAll();
+        void Add(BankAccount account);
+    }
+}
---
+++ Infrastructure/Repositories/InMemoryAccountRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankAccountSystem.Domain.Accounts;
+using BankAccountSystem.Domain.Repositories;
+
+namespace BankAccountSystem.Infrastructure.Repositories
+{
+    public class InMemoryAccountRepository : IAccountRepository
+    {
+        private readonly List<BankAccount> _bankAccounts;
+
+        public InMemoryAccountRepository(List<BankAccount> bankAccounts)
+        {
+            if (bankAccounts == null) throw new ArgumentNullException(nameof(bankAccounts));
+            _bankAccounts = bankAccounts;
+        }
+
+        public void Add(BankAccount account)
+        {
+            if (account == null) throw new ArgumentNullException(nameof(account));
+
+            foreach(BankAccount bankAccount in _bankAccounts)
+            {
+                if (bankAccount.Id == account.Id) throw new InvalidOperationException(nameof(account));
+            }
+            
+            _bankAccounts.Add(account);
+        }
+
+        public IReadOnlyCollection<BankAccount> GetAll()
+        {
+            return new List<BankAccount>(_bankAccounts);
+        }
+
+        public BankAccount GetById(int id)
+        {
+            if (id < 1) throw new ArgumentOutOfRangeException(nameof(id));
+
+            foreach (BankAccount account in _bankAccounts)
+            {
+                if (account.Id == id) return account;
+            }
+
+            return null;
+        }
+    }
+}
