8451246c0b373fae27e63e8376f6821f4cfb305b
Rinxan <bikbulatovravil@mail.ru>
2026-01-12 16:02:22 +0300

    Adding try-catch

---
+++ Domain/Exceptions/NotEnoughMoney.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountSystem.Domain.Exceptions
+{
+    public class NotEnoughMoney(string message) : DomainException(message) { }
+}
--- Program.cs
+++ Program.cs
@@ -5,26 +5,36 @@ using BankAccountSystem.Domain.Services;
 using BankAccountSystem.Infrastructure.Logger;
 using BankAccountSystem.Infrastructure.Repositories;
 
-BankAccount anna = new SavingsAccount(2, "Anna", 100);
-BankAccount masha = new CreditAccount(3, "Masha", 1000);
-BankAccount olga = new SavingsAccount(4, "Olya", 1000000);
-List<BankAccount> bankAccounts = [anna, masha];
-
 string filePath = "D:\\practise\\c#\\BankAccountSystem\\Infrastructure\\log.txt";
-
 ILogger logger = new FileLogger(filePath);
-IAccountRepository bankRepository = new InMemoryAccountRepository(bankAccounts);
-TransferService transferService = new TransferService(bankRepository, logger);
 
-bankRepository.Add(olga);
+try
+{
+    BankAccount anna = new SavingsAccount(2, "Anna", 100);
+    BankAccount masha = new CreditAccount(3, "Masha", 1000);
+    BankAccount olga = new SavingsAccount(4, "Olya", 1000000);
+    List<BankAccount> bankAccounts = [anna, masha];
 
-anna.Deposit(6000);
-masha.Deposit(150000);
 
-Console.WriteLine(anna.PrintInfo());
-Console.WriteLine(masha.PrintInfo());
+    IAccountRepository bankRepository = new InMemoryAccountRepository(bankAccounts);
+    TransferService transferService = new TransferService(bankRepository, logger);
 
-transferService.Transfer(masha.Id, anna.Id, 50000);
+    bankRepository.Add(olga);
 
-Console.WriteLine(anna.PrintInfo());
-Console.WriteLine(masha.PrintInfo());
+    anna.Deposit(6000);
+    masha.Deposit(150000);
+
+    Console.WriteLine(anna.PrintInfo());
+    Console.WriteLine(masha.PrintInfo());
+
+    transferService.Transfer(masha.Id, anna.Id, 50000);
+
+    Console.WriteLine(anna.PrintInfo());
+    Console.WriteLine(masha.PrintInfo());
+} catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    logger.Log(LogLevel.Error, ex.Message);   
+}
+
+Console.WriteLine("Operation success");
